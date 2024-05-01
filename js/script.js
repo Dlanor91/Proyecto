7fe19057eb27b3a10ffb5755ee85b3b06273b930
Dlanor91 <limaronald926@gmail.com>
2024-05-01 10:15:03 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+var nombre ="Ronald";
+var apellido = "Lima";
+var segundoApellido =  "Nunez";
+
+console.log(nombre + apellido + segundoApellido)
+
+var numero = "8"
+
+console.log( numero + 1)//8 string + 1 string + algo = string 81
+console.log( numero - 1)
+console.log( Number(numero) + 1)
+
+var cadena1 = "La mañana se nos echa encima.";
+var cadena2 = cadena1.slice(3, -2);
+console.log(cadena2);
+
+
+
